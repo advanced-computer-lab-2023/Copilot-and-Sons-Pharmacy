4a61fd042271f7c5413e10a6873f352e1265f25e
zahra-saadawy <zahrasaadawy@gmail.com>
2023-10-11 21:13:17 +0300

    View All Patients

--- frontend/src/features/auth/routes/index.tsx
+++ frontend/src/features/auth/routes/index.tsx
@@ -2,6 +2,7 @@ import { RouteObject } from "react-router-dom";
 import { Login } from "./Login";
 import RegistrationForm  from "./RegistrationForm";
 import { Register } from "../../../components/pharmacistRequest";
+import {Formik} from 'formik'
 
 export const authRoutes: RouteObject[] = [
   {
