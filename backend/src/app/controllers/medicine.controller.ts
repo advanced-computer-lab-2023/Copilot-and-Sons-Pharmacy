f3e2b2663badd504a7592c4397c5fb8fc3bf8a6b
Dareenfadel <https://github.com/Dareenfadel>
2023-10-13 21:31:14 +0200

    add Imedicine Type , change registeration error and change add medicine route

--- backend/src/app/controllers/medicine.controller.ts
+++ backend/src/app/controllers/medicine.controller.ts
@@ -1,17 +1,25 @@
-import { Request, Response } from 'express';
-import {SUCCESS,FAIL,ERROR} from '../utils/httpStatusText'
-import {fetchAllMedicines} from '../services/fetchAllMedicines.service'; 
-import asyncWrapper from '../middlewares/asyncWrapper';
-import { viewMedicineQuantityAndSales } from '../services/viewQuantityAndSales.service';
+import { Request, Response } from "express";
+import { SUCCESS, FAIL, ERROR } from "../utils/httpStatusText";
+import { fetchAllMedicines } from "../services/fetchAllMedicines.service";
+import asyncWrapper from "../middlewares/asyncWrapper";
+import { viewMedicineQuantityAndSales } from "../services/viewQuantityAndSales.service";
+import { addMedicineService } from "../services/addMedicine.service";
 
-// Controller function to fetch all medicines
-export const getAllMedicines = asyncWrapper( async ( req: Request,res: Response) => {
-        const medicines = await fetchAllMedicines();
-        res.status(200).json({ success: SUCCESS, data: medicines });}
-     )
-  
+export const getAllMedicines = asyncWrapper(
+  async (req: Request, res: Response) => {
+    const medicines = await fetchAllMedicines();
+    res.status(200).json({ success: SUCCESS, data: medicines });
+  }
+);
 
-export const viewMedicinesQuantityAndSales=asyncWrapper( async ( req: Request,res: Response) => {
- const medicines=await viewMedicineQuantityAndSales();
- res.status(200).json({ success: SUCCESS, data: medicines });
-})
+export const addMedicine = asyncWrapper(async (req: Request, res: Response) => {
+  const medicine = await addMedicineService(req.body);
+  res.json({ success: SUCCESS, data: medicine });
+});
+
+export const viewMedicinesQuantityAndSales = asyncWrapper(
+  async (req: Request, res: Response) => {
+    const medicines = await viewMedicineQuantityAndSales();
+    res.status(200).json({ success: SUCCESS, data: medicines });
+  }
+);
--- backend/src/app/controllers/pharmacist.controller.ts
+++ backend/src/app/controllers/pharmacist.controller.ts
@@ -1,32 +1,23 @@
-import { Request, Response ,NextFunction} from 'express';
-import { addMedicineService } from '../services/addMedicine.service';
-import { SUCCESS } from '../utils/httpStatusText';
-import asyncWrapper from '../middlewares/asyncWrapper';
- import { addPharmacistService } from '../services/addPharmacist.service';
-import { editMedicineService } from '../services/editMedicine.service';
+import { Request, Response, NextFunction } from "express";
+import { SUCCESS } from "../utils/httpStatusText";
+import asyncWrapper from "../middlewares/asyncWrapper";
+import { addPharmacistService } from "../services/addPharmacist.service";
+import { editMedicineService } from "../services/editMedicine.service";
 
+const Pharmacist = require("../schemas/pharmacist");
+const Joi = require("joi");
 
+export const addPharmacist = asyncWrapper(
+  async (req: Request, res: Response) => {
+    const result = await addPharmacistService(req.body);
 
+    return res.json(result);
+  }
+);
 
-const Pharmacist = require('../schemas/pharmacist');
-const Joi = require('joi');
-
-
-export const addPharmacist = asyncWrapper(async (req: Request, res: Response) => {
-const result=await addPharmacistService(req.body);
-
-return res.json(result)
-
-    })
-
-
-        
-        export const addMedicine =asyncWrapper( async ( req: Request,res: Response) => { 
-          const medicine = await addMedicineService(req.body);
-          res.json({ success: SUCCESS, data: medicine });
-        })
-
-        export const editMedicine =asyncWrapper( async ( req: Request,res: Response) => { 
-          const medicine = await editMedicineService(req.body.name,req.body.edits);
-          res.json({ success: SUCCESS, data: medicine });
-        })
+export const editMedicine = asyncWrapper(
+  async (req: Request, res: Response) => {
+    const medicine = await editMedicineService(req.body.name, req.body.edits);
+    res.json({ success: SUCCESS, data: medicine });
+  }
+);
