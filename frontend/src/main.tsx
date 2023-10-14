6c9e534d6739c4ea9d4af7934b314891e1b9d998
zahra-saadawy <zahrasaadawy@gmail.com>
2023-10-14 18:51:16 +0300

    Fixed search for medicine to search by substring and implemented its frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
 import { routes } from "./routes";
 // import './index.css';
 
