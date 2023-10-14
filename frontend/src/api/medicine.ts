abc98313086b07d9e97d2d9d2eef535448d4bd4c
Youssof Kandil <yaskm2002@gmail.com>
2023-10-14 21:34:27 +0300

    Update medicine.ts

--- frontend/src/api/medicine.ts
+++ frontend/src/api/medicine.ts
@@ -1,3 +1,5 @@
+import { useMutation } from 'react-query';
+import  { IMedicine } from './../../../backend/src/app/schemas/medicine.model';
 import axios, { AxiosResponse } from 'axios';
 
 
@@ -16,6 +18,31 @@ export async function viewAllMedicines(): Promise<AxiosResponse> {
 
 
   } 
+//Calling the add Medicine function using axios
+  function addMedicineService(Medicine:IMedicine) {
+    console.log("I'm herreeee")
+    console.log(Medicine)
+    return axios.post(`${BASE_URL}/api/medicine/addMedicine`, Medicine ).then((response) => response)
+    .catch((error) => {throw new Error(error)});
+  }
+  //setting a mutation using react_query to handle the changes to database
+  // exporting it to use in the actual component
+  export function useAddMedicineService() {
+    return useMutation(addMedicineService);
+
+  }
+
+  //Calling the editMedicine function using axios
+  function editMedicineService(Medicine) {
+    return axios.put(`${BASE_URL}/api/medicine/editMedicine/${Medicine.name}`, Medicine ).then((response) => response)
+    .catch((error) => {throw new Error(error)});
+  }
+  //setting a mutation using react_query to handle the changes to database
+  // exporting it to use in the actual component
+  export function useEditMedicineService() {
+    return useMutation(editMedicineService);
+
+  }
 
   export async function searchForMedicine(name: string) {
     const response = await axios.get(`${BASE_URL}/api/admin/getMedicineByName/${name}`);
@@ -27,9 +54,8 @@ export async function viewAllMedicines(): Promise<AxiosResponse> {
     const response = await axios.get(`${BASE_URL}/api/medicine/allMedicinalUses`);
     return response.data;
   }
+
   export async function filterByMedicinalUse(name:string) {
     const response = await axios.get(`${BASE_URL}/api/medicine/filterByMedicinalUse/${name}`);
-
-
     return response.data;
   }
