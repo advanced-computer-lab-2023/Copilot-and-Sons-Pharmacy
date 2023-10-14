1c065dce211888d85da01ae778a64f542ed3ebbb
Youssof Kandil <yaskm2002@gmail.com>
2023-10-14 21:31:23 +0300

    Update index.tsx

--- frontend/src/features/medicine-dashboard/routes/index.tsx
+++ frontend/src/features/medicine-dashboard/routes/index.tsx
@@ -5,7 +5,8 @@ import ViewMedicineSalesAndQuantity from './ViewMedicineSalesAndQuantity'
 import SearchForMedicine from './searchForMedicine'
 import MedicinalUses from './ViewAllMedicinalUses'
 import FilteredMedicines from './FilterMedicines'
-
+import { EditMedicine } from './EditMedicine'
+import { AddMedicine } from './AddMedicine'
 export const medicineRoutes: RouteObject[] = [
   {
     element: <MedicineDashboardLayout />,
@@ -21,6 +22,14 @@ export const medicineRoutes: RouteObject[] = [
       {
         path: 'search-for-medicine',
         element: <SearchForMedicine/>,
+      },
+      {
+        path: 'editMedicine/:name',
+        element: <EditMedicine/>,
+      },
+       {
+        path: 'addMedicine',
+        element: < AddMedicine />,
       },
       {
         path: 'allUses',
