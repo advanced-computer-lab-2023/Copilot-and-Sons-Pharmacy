53b10e8d271db3ba6baaec612df9860deec92b8c
Mathew Hany <mathew.hanybb@gmail.com>
2023-10-30 19:47:01 +0200

    Add ESLint, Docker, Git Hooks, GitHub Actions, Prettier

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,20 +1,22 @@
-import mongoose, { Connection } from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
-const url: string = process.env.Mongo_URL||"";
+import mongoose, { Connection } from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+const url: string = process.env.MONGO_URI || process.env.Mongo_URL || ''
+
 // Connect to MongoDB
 try {
   mongoose.connect(url).then(() => {
-    console.log("connected to server");
-  });
+    console.log('connected to server')
+  })
 } catch (error) {
-  console.error("MongoDB connection error:", error);
+  console.error('MongoDB connection error:', error)
 }
 
-const db: Connection = mongoose.connection;
+const db: Connection = mongoose.connection
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
-export default db;
+  console.log('Connected to MongoDB')
+})
+
+export default db
