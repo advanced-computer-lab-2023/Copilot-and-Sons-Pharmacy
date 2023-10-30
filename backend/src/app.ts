49bfefdd0732c5ad8a140602a24e66306587c515
Mathew Hany <mathew.hanybb@gmail.com>
2023-10-30 21:28:40 +0200

    Add endpoints to create fake data for testing  + seed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,7 @@ import patientsRoute from './app/routes/patient.route'
 import adminsRoute from './app/routes/admin.route'
 import pharmacistRoute from './app/routes/pharmacist.route'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
+import debugRouter from './app/controllers/debug.controller'
 const app: Application = express()
 
 app.use(cors())
@@ -14,6 +15,8 @@ app.use('/api/medicine', medicinesRoute)
 app.use('/api/patient', patientsRoute)
 app.use('/api/admin', adminsRoute)
 app.use('/api/pharmacist', pharmacistRoute)
+app.use('/api/debug', debugRouter)
+
 //global error handler
 app.use(globalErrorHandler)
 
