4416db448dbceb96b2a47efe251dbb1b07214753
ramezlahzy <rameznashaat9999@gmail.com>
2023-11-08 21:24:55 +0200

    FEATURE/FE-strip

--- frontend/src/features/patient-dashboard/routes/Checkout.tsx
+++ frontend/src/features/patient-dashboard/routes/Checkout.tsx
@@ -1,29 +1,28 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
-import {loadStripe} from '@stripe/stripe-js';
+import React, { useState } from 'react';
+import {loadStripe, StripeElementsOptions} from '@stripe/stripe-js';
 import {
-    PaymentElement,
     Elements,
     useStripe,
     useElements,
+    PaymentElement,
 } from '@stripe/react-stripe-js';
 import Button from '@mui/material/Button';
 
-const CheckoutForm = () => {
+const CheckoutForm: React.FC = () => {
     const stripe = useStripe();
     const elements = useElements();
 
-    const [errorMessage, setErrorMessage] = useState(null);
+    const [errorMessage, setErrorMessage] = useState<string | undefined>();
 
-    const handleSubmit = async (event) => {
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
-        if (elements == null) {
+        if (!elements) {
             return;
         }
 
         // Trigger form validation and wallet collection
-        const {error: submitError} = await elements.submit();
+        const { error: submitError } = await elements.submit();
         if (submitError) {
             // Show error to your customer
             setErrorMessage(submitError.message);
@@ -35,22 +34,21 @@ const CheckoutForm = () => {
             method: 'POST',
         });
 
-        const {client_secret: clientSecret} = await res.json();
+        const { client_secret: clientSecret } = await res.json();
 
-        const {error} = await stripe.confirmPayment({
-            //`Elements` instance that was used to create the Payment Element
+        const { error } = await stripe?.confirmPayment({
             elements,
             clientSecret,
             confirmParams: {
                 return_url: 'https://example.com/order/123/complete',
             },
-        });
+        }) ?? {};
 
         if (error) {
             // This point will only be reached if there is an immediate error when
             // confirming the payment. Show error to your customer (for example, payment
             // details incomplete)
-            setErrorMessage(error.message);
+            setErrorMessage(error?.message);
         } else {
             // Your customer will be redirected to your `return_url`. For some payment
             // methods like iDEAL, your customer will be redirected to an intermediate
@@ -60,12 +58,12 @@ const CheckoutForm = () => {
 
     return (
         <form onSubmit={handleSubmit}>
-            <PaymentElement/>
-            {/*<button type="submit" disabled={!stripe || !elements}>*/}
-            {/*    Pay*/}
-            {/*</button>*/}
-            <Button variant="contained" type="submit" disabled={!stripe || !elements}
-                    style={{backgroundColor: '#3f51b5', color: 'white',marginTop: 10}}
+            <PaymentElement />
+            <Button
+                variant="contained"
+                type="submit"
+                disabled={!stripe || !elements}
+                style={{ backgroundColor: '#3f51b5', color: 'white', marginTop: 10 }}
             >
                 Pay
             </Button>
@@ -77,19 +75,19 @@ const CheckoutForm = () => {
 
 const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
 
-const options = {
+const options:StripeElementsOptions = {
     mode: 'payment',
     amount: 1099,
     currency: 'usd',
-    // Fully customizable with appearance API.
+    // Fully customizable with the appearance API.
     appearance: {
         /*...*/
     },
 };
 
-const Checkout = () => (
+const Checkout: React.FC = () => (
     <Elements stripe={stripePromise} options={options}>
-        <CheckoutForm/>
+        <CheckoutForm />
     </Elements>
 );
 
