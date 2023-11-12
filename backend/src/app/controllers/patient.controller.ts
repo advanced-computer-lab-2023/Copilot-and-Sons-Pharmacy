27f4ab50852681a5f16f1ea4044769e20a144aa7
zahra-saadawy <zahrasaadawy@gmail.com>
2023-11-12 16:36:39 +0200

    fixed failed github tests

--- backend/src/app/controllers/patient.controller.ts
+++ backend/src/app/controllers/patient.controller.ts
@@ -1,4 +1,3 @@
-import { Request, Response } from 'express'
 import registerService from '../services/register.service'
 import { SUCCESS } from '../utils/httpStatusText'
 import asyncWrapper from '../middlewares/asyncWrapper'
@@ -9,7 +8,7 @@ import { sendOTP, verifyOTP, updatePassword } from '../services/forgotPassowrd'
 import { ERROR } from './../utils/httpStatusText'
 import AppError from '../utils/appError'
 
-export const requestOTP = asyncWrapper(async (req: Request, res: Response) => {
+export const requestOTP = asyncWrapper(async (req, res) => {
   const { email } = req.body
 
   if (email) {
@@ -18,79 +17,69 @@ export const requestOTP = asyncWrapper(async (req: Request, res: Response) => {
   }
 })
 
-export const verifyOTPController = asyncWrapper(
-  async (req: Request, res: Response) => {
-    console.log('heyy i entered')
-    const { otp, email } = req.body
+export const verifyOTPController = asyncWrapper(async (req, res) => {
+  console.log('heyy i entered')
+  const { otp, email } = req.body
 
-    const isOTPValid = await verifyOTP(email, otp)
+  const isOTPValid = await verifyOTP(email, otp)
 
-    if (isOTPValid) {
-      res.json({ success: SUCCESS, message: 'OTP verified successfully' })
+  if (isOTPValid) {
+    res.json({ success: SUCCESS, message: 'OTP verified successfully' })
+  } else {
+    res.json({ error: ERROR, message: 'not verified' })
+  }
+})
+
+export const updatePasswordController = asyncWrapper(async (req, res) => {
+  const { newPassword, email } = req.body
+
+  if (!email) {
+    res.status(400).json({ error: 'Email not provided' })
+
+    return
+  }
+
+  try {
+    const result = await updatePassword(email, newPassword)
+    res.json({ success: SUCCESS, message: result })
+  } catch (error) {
+    if (error instanceof AppError) {
+      res.status(error.statusCode).json({ error: error.message })
     } else {
-      res.json({ error: ERROR, message: 'not verified' })
+      res.status(500).json({ error: 'Internal Server Error' })
     }
   }
-)
+})
 
-export const updatePasswordController = asyncWrapper(
-  async (req: Request, res: Response) => {
-    const { newPassword, email } = req.body
+export const changeUserPassword = asyncWrapper(async (req, res) => {
+  const { oldPassword, newPassword } = req.body
+  const username = req.username
+  await changePassowrd(username!, oldPassword, newPassword)
+  console.log(username)
+  res.json({
+    success: SUCCESS,
+    message: 'Password changed successfulu',
+  })
+})
 
-    if (!email) {
-      res.status(400).json({ error: 'Email not provided' })
+export const getAllPatients = asyncWrapper(async (req, res) => {
+  console.log('hi i am here')
+  const patients = await fetchAllPatients()
 
-      return
-    }
-
-    try {
-      const result = await updatePassword(email, newPassword)
-      res.json({ success: SUCCESS, message: result })
-    } catch (error) {
-      if (error instanceof AppError) {
-        res.status(error.statusCode).json({ error: error.message })
-      } else {
-        res.status(500).json({ error: 'Internal Server Error' })
-      }
-    }
+  if (!patients) {
+    console.log('not found')
   }
-)
 
-export const changeUserPassword = asyncWrapper(
-  async (req: Request, res: Response) => {
-    const { oldPassword, newPassword } = req.body
-    const username = req.username
-    await changePassowrd(username!, oldPassword, newPassword)
-    console.log(username)
-    res.json({
-      success: SUCCESS,
-      message: 'Password changed successfulu',
-    })
-  }
-)
+  console.log(patients)
+  res.status(200).json({ success: SUCCESS, data: patients })
+})
 
-export const getAllPatients = asyncWrapper(
-  async (req: Request, res: Response) => {
-    console.log('hi i am here')
-    const patients = await fetchAllPatients()
+export const registerController = asyncWrapper(async (req, res) => {
+  const patient = await registerService(req.body)
+  res.status(201).json({ status: SUCCESS, data: patient })
+})
 
-    if (!patients) {
-      console.log('not found')
-    }
-
-    console.log(patients)
-    res.status(200).json({ success: SUCCESS, data: patients })
-  }
-)
-
-export const registerController = asyncWrapper(
-  async (req: Request, res: Response) => {
-    const patient = await registerService(req.body)
-    res.status(201).json({ status: SUCCESS, data: patient })
-  }
-)
-
-export const getPatient = asyncWrapper(async (req: Request, res: Response) => {
+export const getPatient = asyncWrapper(async (req, res) => {
   const patient = await getPatientByUsername(req.params.username)
   res.status(201).json({ status: SUCCESS, data: patient })
 })
