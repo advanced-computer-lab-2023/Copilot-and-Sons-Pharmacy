425e0fba49e8e02b17cb84f62e03cf8e482cb971
Dareenfadel <https://github.com/Dareenfadel>
2023-11-29 15:21:39 +0200

    remove unused controller

--- backend/src/app/routes/prescription.route.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { prescriptionController } from '../controllers/prescription.controller'
-
-const router = express.Router()
-router.use(express.json())
-router.route('/add-medicine').post(prescriptionController)
-
-export default router
