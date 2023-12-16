2491254769d72600c2d1934737868023263d9df5
Dareenfadel <https://github.com/Dareenfadel>
2023-12-16 14:58:29 +0200

    doctor has option to get back to clinic

--- frontend/src/features/doctor-dashboard/components/DoctorDashboardLayout.tsx
+++ frontend/src/features/doctor-dashboard/components/DoctorDashboardLayout.tsx
@@ -5,6 +5,9 @@ import { Outlet } from 'react-router-dom'
 import { AuthenticatedRoute } from '@/components/AuthenticatedRoute'
 import { UserType } from 'pharmacy-common/types/user.types'
 import { Healing } from '@mui/icons-material'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStethoscope } from '@fortawesome/free-solid-svg-icons'
+import MessageIcon from '@mui/icons-material/Message'
 
 export function DoctorDashboardLayout() {
   const { setSidebarLinks } = useSidebar()
@@ -19,7 +22,12 @@ export function DoctorDashboardLayout() {
       {
         to: '/Doctor-dashboard/pharmacists',
         text: 'Chat with Pharmacists',
-        icon: <Healing />,
+        icon: <MessageIcon />,
+      },
+      {
+        to: '/doctor-dashboard/clinic',
+        text: 'Your Clinic',
+        icon: <FontAwesomeIcon icon={faStethoscope} />,
       },
     ])
   }, [setSidebarLinks])
