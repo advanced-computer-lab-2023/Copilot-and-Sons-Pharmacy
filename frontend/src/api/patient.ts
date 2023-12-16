bca18b005e439bc2665c7887490ebca7018611ef
youssef1shawky <youssefshawky2002@gmail.com>
2023-12-17 00:13:30 +0200

    fixing view patient & adding data grid

--- frontend/src/api/patient.ts
+++ frontend/src/api/patient.ts
@@ -21,6 +21,13 @@ export async function viewPatientInfo(id: string) {
   return response.data.data
 }
 
+export async function viewPatientInfo2(id: string) {
+  console.log('hey I entered')
+  const response = await api.get(`/admin/patientInfo/${id}`)
+
+  return response.data
+}
+
 export async function addPrescriptionToCart(prescriptionId: any) {
   return api.post('/cart/addPrescriptiontoCart', { prescriptionId })
 }
--- frontend/src/features/patient-dashboard/routes/viewPatient.tsx
+++ frontend/src/features/patient-dashboard/routes/viewPatient.tsx
@@ -1,6 +1,6 @@
 import { Container, Card, CardContent, Typography } from '@mui/material'
 import { useQuery } from 'react-query'
-import { viewPatientInfo } from '../../../api/patient'
+import { viewPatientInfo2 } from '../../../api/patient'
 import { useParams } from 'react-router-dom'
 
 const PatientInfo = () => {
@@ -9,35 +9,58 @@ const PatientInfo = () => {
     data: patient,
     isLoading,
     isError,
-  } = useQuery('patient', () => viewPatientInfo(id!))
-  console.log(patient)
+  } = useQuery('patient', () => viewPatientInfo2(id!))
+
+  const formatDate = (dateString: any) => {
+    const date = new Date(dateString)
+
+    return date.toLocaleDateString('en-US', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    })
+  }
 
   return (
-    <Container>
+    <Container style={{ height: '500px', width: '100%' }}>
       {isLoading && <p>Loading...</p>}
       {isError && <p>Error fetching patient</p>}
       {patient && (
-        <Card variant="outlined">
+        <Card variant="outlined" style={{ height: '100%' }}>
           <CardContent>
-            <Typography variant="h6" component="div">
+            <Typography
+              variant="h4"
+              component="div"
+              sx={{ marginBottom: '20px' }}
+            >
               Patient Information
             </Typography>
 
-            <Typography>Email: {patient.data.email}</Typography>
-            <Typography>Name: {patient.data.name}</Typography>
-            <Typography>Date of Birth: {patient.data.dateOfBirth}</Typography>
-            <Typography>Gender: {patient.data.gender}</Typography>
-            <Typography>Mobile Number: {patient.data.mobileNumber}</Typography>
-            <Typography>
+            <Typography sx={{ fontSize: '20px', marginBottom: '10px' }}>
+              Name: {patient.data.name}
+            </Typography>
+            <Typography sx={{ fontSize: '20px', marginBottom: '10px' }}>
+              Email: {patient.data.email}
+            </Typography>
+            <Typography sx={{ fontSize: '20px', marginBottom: '10px' }}>
+              Date of Birth: {formatDate(patient.data.dateOfBirth)}
+            </Typography>
+            <Typography sx={{ fontSize: '20px', marginBottom: '10px' }}>
+              Gender: {patient.data.gender}
+            </Typography>
+            <Typography sx={{ fontSize: '20px', marginBottom: '10px' }}>
+              Mobile Number: {patient.data.mobileNumber}
+            </Typography>
+            <Typography sx={{ fontSize: '20px', marginBottom: '10px' }}>
               Emergency Contact: {patient.data.emergencyContact.fullName}
             </Typography>
-            <Typography>
+            <Typography sx={{ fontSize: '20px', marginBottom: '10px' }}>
               Relation: {patient.data.emergencyContact.relation}
             </Typography>
-            <Typography>
+            <Typography sx={{ fontSize: '20px', marginBottom: '10px' }}>
               Contact Number: {patient.data.emergencyContact.mobileNumber}
             </Typography>
-          </CardContent>{' '}
+          </CardContent>
         </Card>
       )}
     </Container>
