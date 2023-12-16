5be74d0546710793bae1a666ff7c76e250a884e2
zahra-saadawy <zahrasaadawy@gmail.com>
2023-12-16 23:04:22 +0200

    gave auth to only accepted pharmacists

--- frontend/src/features/pharmacist-dashboard/routes/pharmacistDashboardHome.tsx
+++ frontend/src/features/pharmacist-dashboard/routes/pharmacistDashboardHome.tsx
@@ -8,6 +8,7 @@ import { Copyright } from '@mui/icons-material'
 import { Grid, Paper } from '@mui/material'
 import Deposits from '../components/Deposits'
 import { AlertsBox } from '@/components/AlertsBox'
+import { PharmacistStatus } from 'pharmacy-common/types/pharmacist.types'
 
 export function PharmacistDashboardHome() {
   const { user } = useAuth()
@@ -31,54 +32,56 @@ export function PharmacistDashboardHome() {
 
   return (
     <>
-      <div>
-        <Container maxWidth="lg" sx={{ mt: 4, mb: 1 }}>
-          <Grid
-            container
-            spacing={4}
-            justifyContent="center"
-            paddingBottom={4}
-            paddingRight={6}
-            flexDirection={'column'}
-          >
-            <Grid item xs={12} md={12} lg={12}>
-              <Paper
-                sx={{
-                  p: 2,
-                  display: 'flex',
-                  flexDirection: 'column',
-                  height: 310,
-                  borderRadius: '17px',
-                  color: 'white ',
-                  bgcolor: 'primary.main',
-                }}
-              >
-                <Deposits wallet={res?.pharmacist?.walletMoney} />
-              </Paper>
+      {res.pharmacist.status === PharmacistStatus.Accepted && (
+        <div>
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 1 }}>
+            <Grid
+              container
+              spacing={4}
+              justifyContent="center"
+              paddingBottom={4}
+              paddingRight={6}
+              flexDirection={'column'}
+            >
+              <Grid item xs={12} md={12} lg={12}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: 310,
+                    borderRadius: '17px',
+                    color: 'white ',
+                    bgcolor: 'primary.main',
+                  }}
+                >
+                  <Deposits wallet={res?.pharmacist?.walletMoney} />
+                </Paper>
+              </Grid>
+              <Grid item xs={12} md={12} lg={12}>
+                <Paper
+                  sx={{
+                    paddingX: 2,
+                    paddingY: 2,
+                    height: 'fit-content',
+                    borderRadius: '17px',
+                    overflow: 'auto',
+                    width: '100%',
+                    scrollbarWidth: 'thin', // For Firefox
+                    '&::-webkit-scrollbar': {
+                      width: '0em', // Set the width of the scrollbar
+                    },
+                  }}
+                >
+                  <AllChats />
+                </Paper>
+              </Grid>
             </Grid>
-            <Grid item xs={12} md={12} lg={12}>
-              <Paper
-                sx={{
-                  paddingX: 2,
-                  paddingY: 2,
-                  height: 'fit-content',
-                  borderRadius: '17px',
-                  overflow: 'auto',
-                  width: '100%',
-                  scrollbarWidth: 'thin', // For Firefox
-                  '&::-webkit-scrollbar': {
-                    width: '0em', // Set the width of the scrollbar
-                  },
-                }}
-              >
-                <AllChats />
-              </Paper>
-            </Grid>
-          </Grid>
 
-          <Copyright sx={{ pt: 4 }} />
-        </Container>
-      </div>
+            <Copyright sx={{ pt: 4 }} />
+          </Container>
+        </div>
+      )}
     </>
   )
 }
